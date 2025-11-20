3e173b7948a743c29a57539d96e9c9921b4b5c2d
Andrey Arama <zekmaro@yandex.ru>
2025-11-20 18:30:07 +0100

    feat: 2015 day 5

--- 2015/day05/day05.py
+++ 2015/day05/day05.py
@@ -39,7 +39,34 @@ def part1(data):
 
 
 def part2(data):
-	pass
+	counter = 0
+	for s in data:
+		doubles_cond = False
+		triples_cond = False
+		pairs = {}
+		for i in range(len(s)):
+
+			if i >= 2 and s[i] == s[i - 2]:
+				triples_cond = True
+
+			if i >= 1:
+				pair = s[i-1:i+1]
+				if pair not in pairs:
+					pairs[pair] = []
+				pairs[pair].append(i - 1)
+
+		# not effitient loop
+		for positions in pairs.values():
+			for i in range(len(positions)):
+				for j in range(i + 1, len(positions)):
+					if positions[j] - positions[i] >= 2:
+						doubles_cond = True
+						break
+
+		if doubles_cond and triples_cond:
+			counter += 1
+
+	return counter
 
 
 if __name__ == "__main__":

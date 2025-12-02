2c55e966272910ff5ce7594d013a87c3a21b7859
Andrey Arama <zekmaro@yandex.ru>
2025-12-02 07:08:45 +0100

    feat: garbage

--- 2025/day02.py
+++ 2025/day02.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import time
 from pathlib import Path
+from itertools import combinations
 
 DAY = Path(__file__).stem  # "day01"
 
@@ -18,21 +19,52 @@ def parse(data):
     Example:
         return [int(x) for x in data.splitlines()]
     """
-    return data.splitlines()
+    return data.split(',')
 
 
 def part1(data):
     """
     Solve part 1 using parsed data.
     """
-    return None
-
+    invalid = 0
+    for s in data:
+        a, b = s.split("-")
+        a = int(a)
+        b = int(b)
+        for n in range(a, b + 1):
+            temp = str(n)
+            l = len(temp)
+            first = temp[0: l//2]
+            second = temp[l//2:]
+            if first == second and first[0] != '0' and second[0] != '0':
+                # print('IN')
+                invalid += int(n)
+    return invalid
+            
+def is_repeated_block(s):
+    n = len(s)
+    for k in range(1, n // 2 + 1):
+        if n % k == 0:
+            block = s[:k]
+            if block * (n // k) == s:
+                return True
+    return False
 
 def part2(data):
     """
     Solve part 2 using parsed data.
     """
-    return None
+    invalid = 0
+    for s in data:
+        a, b = s.split("-")
+        a = int(a)
+        b = int(b)
+        for n in range(a, b + 1):
+            temp = str(n)
+            if is_repeated_block(temp):
+                invalid += int(n)
+
+    return invalid
 
 # ---- RUNNER ----------------------------------------------------------------
 
@@ -40,14 +72,14 @@ def run():
     data_raw = read_input()
     data = parse(data_raw)
 
-    t0 = time.time()
-    p1 = part1(data)
+    # t0 = time.time()
+    # p1 = part1(data)
     t1 = time.time()
     p2 = part2(data)
     t2 = time.time()
 
     print(f"Day {DAY[-2:]}:")
-    print(f"  Part 1: {p1}  ({(t1 - t0)*1000:.3f} ms)")
+    # print(f"  Part 1: {p1}  ({(t1 - t0)*1000:.3f} ms)")
     print(f"  Part 2: {p2}  ({(t2 - t1)*1000:.3f} ms)")
 
 

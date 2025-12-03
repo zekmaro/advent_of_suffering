3f78c5b6f689e1324122c719f9792a2c2720578e
Andrey Arama <zekmaro@yandex.ru>
2025-12-03 07:31:08 +0100

    feat: ignore + day 3. stack + greedy

--- 2025/day03.py
+++ 2025/day03.py
@@ -25,14 +25,48 @@ def part1(data):
     """
     Solve part 1 using parsed data.
     """
-    return None
+    total = 0
+    for line in data:
+        best = -1
+        for i in range(len(line)):
+            for j in range(i + 1, len(line)):
+                d1 = int(line[i])
+                d2 = int(line[j])
+                pair = d1 * 10 + d2
+                best = max(best, pair)
+
+        total += best
+    return total
 
 
 def part2(data):
     """
     Solve part 2 using parsed data.
     """
-    return None
+    total = 0
+    for line in data:
+        stack = []
+        n = len(line)
+        k = 12
+        out = n - k
+        for i in range(n):
+            while stack and stack[-1] < line[i] and out > 0:
+                stack.pop()
+                out -= 1
+
+            stack.append(line[i])
+        
+        while out > 0:
+            stack.pop()
+            out -= 1
+
+        num = 0
+        for d in stack:
+            num = num * 10 + int(d)
+        
+        total += num
+        
+    return total
 
 # ---- RUNNER ----------------------------------------------------------------
 
@@ -40,14 +74,14 @@ def run():
     data_raw = read_input()
     data = parse(data_raw)
 
-    t0 = time.time()
-    p1 = part1(data)
+    # t0 = time.time()
+    # p1 = part1(data)
     t1 = time.time()
     p2 = part2(data)
     t2 = time.time()
 
     print(f"Day {DAY[-2:]}:")
-    print(f"  Part 1: {p1}  ({(t1 - t0)*1000:.3f} ms)")
+    # print(f"  Part 1: {p1}  ({(t1 - t0)*1000:.3f} ms)")
     print(f"  Part 2: {p2}  ({(t2 - t1)*1000:.3f} ms)")
 
 
